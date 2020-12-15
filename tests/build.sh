b5487b5da1ae383ebe466bfdc2405578d42db8dd
Natan Votre <nvt@brickabode.com>
2020-12-14 22:49:09 -0300

    Add CI

---
+++ tests/build.sh
@@ -0,0 +1,15 @@
+SRC_PATH=$(pwd)
+VERILOG_FILES=$(find $SRC_PATH -name "*.v")
+TOPLEVEL="max10"
+
+HAS_ERROR=0
+iverilog -s $TOPLEVEL $VERILOG_FILES
+RESULT=$(echo $?)
+if [ $RESULT -eq 0 ]
+then
+  echo "$(echo "$VERILOG_FILES" | wc -l)\e[90m Files \e[92mCompiled\e[39m!"
+else
+  HAS_ERROR=1
+fi
+
+exit $HAS_ERROR
